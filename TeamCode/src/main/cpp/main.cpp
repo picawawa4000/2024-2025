4314cc64c2c0075f502855121746850a243cc7ae
picawawa4000 <114023995+picawawa4000@users.noreply.github.com>
2024-09-07 14:13:28 -0400

    this is already not going well

---
+++ TeamCode/src/main/cpp/main.cpp
@@ -0,0 +1,11 @@
+//Don't forget to configure the file settings in CMakeLists.txt when adding a library!
+
+#include <jni.h>
+#include <libcardinal.hpp>
+
+extern "C" JNIEXPORT void JNICALL Java_org_firstinspires_ftc_teamcode_RemoteOperation_run(
+        JNIEnv *env, jobject thiz) {
+    // TODO: implement run()
+    jmethodID jmethodId = env->GetMethodID(env->GetObjectClass(thiz), "doStuff", "(I)V");
+    env->CallVoidMethod(thiz, jmethodId, 3);
+}
